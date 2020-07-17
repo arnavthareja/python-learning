e12f68d9288d1053a4e63f3f2758dd2b010e2ae5
arnavthareja <arnav.thareja25@gmail.com>
2020-07-16 21:38:15 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+# %%
+print(2)
+
+
+# %%
+print(5)
+
+# %%
+import numpy as np
+print(np.floor(2.7))
+print(3**4)
+
+# %%
+def helloWorld(string = 'hello world'):
+    print(string)
+
+helloWorld()
+helloWorld('hello again')
+
+# %%
+helloWorld('hello a third time')
+
+# %%
